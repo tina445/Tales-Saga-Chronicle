0ca4742e0c56fb7d1649cf79bcfa4391e55188aa
tina445 <113275566+tina445@users.noreply.github.com>
2024-12-28 03:53:01 +0900

    [TEST] github webhook Test
    github 웹후크 커밋 메시지 테스트 커밋
    Test Commit for github webhook test

--- Tales Saga Chronicle/Assets/Scripts/Temp.cs
+++ Tales Saga Chronicle/Assets/Scripts/Temp.cs
@@ -5,7 +5,7 @@ public class Temp : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("hello world!");
     }
 
     // Update is called once per frame
